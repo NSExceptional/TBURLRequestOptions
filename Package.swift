f84f56232af7dc80c5e21f55cc22026ade7a8ce7
Tanner Bennett <tannerbennett@me.com>
2021-10-03 02:03:48 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "TBURLRequestOptions",
+    products: [
+        .library(
+            name: "TBURLRequestOptions",
+            targets: ["TBURLRequestOptions"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "TBURLRequestOptions",
+            dependencies: [],
+            path: "TBURLRequestOptions/Classes"
+        )
+    ]
+)
